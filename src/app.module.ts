3dbd70acd58b581d77ce61a610724b1e21851011
sidgaikwad <gaikwadsiddharth03@gmail.com>
2024-02-06 17:54:18 +0530

    same changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
 })
 export class AppModule {}
